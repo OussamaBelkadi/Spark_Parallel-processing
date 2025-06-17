f5101369c23d38c479f7eed43b371255dc69ac76
Oussama1Belkadi <oussama.belkadi@symolia.com>
2025-06-17 11:56:15 +0100

    new feature

--- src/main/java/com/example/sprak_tp_sp/PrixTotalVentesParVilleEtAnnee.java
+++ src/main/java/com/example/sprak_tp_sp/PrixTotalVentesParVilleEtAnnee.java
@@ -1,6 +1,10 @@
 package com.example.sprak_tp_sp;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 public class PrixTotalVentesParVilleEtAnnee {
 
